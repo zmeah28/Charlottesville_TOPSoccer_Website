5c96985262b953187bcc415eb9e1d14dd601a741
Evin St Clair <evin@MacBook-Air-3.local>
2025-05-26 12:59:46 -0700

    [Bug fix]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,13 @@
 module.exports = {
     root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      project: './tsconfig.json'
-    },
-    env: {
-      browser: true,
-      node: true,
-      es2021: true
-    },
-    plugins: [
-      '@typescript-eslint',
-      'import',
-      'react',
-      'react-hooks'
+    ignorePatterns: [
+      'node_modules/',
+      '.next/',
+      'public/',
+      'src/generated/prisma/**',    
     ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'next/core-web-vitals'
-    ],
+    extends: ['next/core-web-vitals'],
     rules: {
-      '@typescript-eslint/no-var-requires': 'off',
-      'import/no-commonjs': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
-      'react/react-in-jsx-scope': 'off'
     },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
   }
   
